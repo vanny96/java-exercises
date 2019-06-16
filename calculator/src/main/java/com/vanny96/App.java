00f6edd2173ebb5ec8dae09b6386b3128406f9e7
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-06-16 15:22:11 +0200

    Complete the calculator

--- calculator/src/main/java/com/vanny96/App.java
+++ calculator/src/main/java/com/vanny96/App.java
@@ -1,6 +1,8 @@
 package com.vanny96;
 
 import java.util.Scanner;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class App 
 {
@@ -8,6 +10,53 @@ public class App
     {
         Scanner reader = new Scanner(System.in);       
 
+        int numbers[] = getNumbers(reader);
+
+        // Get method 
+        System.out.println("Enter the method");
+        String methodName = reader.nextLine();
+
+        // Find the method
+        App thisObject = new App();
+        Class thisClass = thisObject.getClass();
+
+        Class[] getMethodArguments = new Class[2];
+        getMethodArguments[0] = int.class;
+        getMethodArguments[1] = int.class;
+
+        Method method;
+
+        // Invoke the method
+        try {
+            method = thisClass.getMethod(methodName, getMethodArguments);
+            try {
+                int result =(int) method.invoke(methodName, numbers[0], numbers[1]);
+                System.out.println(result);
+                
+                
+            } catch (IllegalAccessException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IllegalArgumentException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } catch (NoSuchMethodException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+       
+    }
+
+
+    private static int[] getNumbers(Scanner reader){
         System.out.println( "Type two numbers" );
         String[] strgNumbers = reader.nextLine().split(" ");
         int[] numbers = new int[2];
@@ -15,21 +64,23 @@ public class App
         for(int i=0; i<2; i++){
             numbers[i] = Integer.parseInt(strgNumbers[i]);
         }
+
+        return numbers;
     }
 
-    private Integer sum(int num1, int num2){
+    public static Integer sum(int num1, int num2){
         return num1 + num2;
     }
 
-    private Integer difference(int num1, int num2){
+    public static Integer difference(int num1, int num2){
         return num1 - num2;
     }
 
-    private Integer multiplication(int num1, int num2){
+    public static Integer multiplication(int num1, int num2){
         return num1 * num2;
     }
 
-    private Integer division(int num1, int num2){
+    public static Integer division(int num1, int num2){
         return num1 / num2;
     }
 }
